c5bc1008cdf369d27e1ffaf79ee96e8a6e7c1edf
Jose Antonio Rojas <game4droidplay@gmail.com>
2020-10-13 12:57:49 -0400

    Conversor de monedas v1

---
+++ conversor.py
@@ -0,0 +1,7 @@
+pesos = input("Cuantos pesos tienes?: ")
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("Tienes esta cantidades de dolares: "+ dolares)
