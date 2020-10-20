d3ba1deeb83d3f78b3f0f5e0328a10019675a60a
maria carrero <mariacarrerochacon267@gmail.com>
2020-10-20 09:39:55 -0500

    modifique el ejercicio de primalidad para hacerlo mas simple y para que no tomara el 1 como primo

--- prueba_primalidad.py
+++ prueba_primalidad.py
@@ -1,12 +1,10 @@
 def es_primo (numero):
     contador = 0
-    for i in range (1, numero + 1):
-        if i == 1 or i == numero:
-            continue
-        if numero % i == 0:
+    for i in range (2, numero + 1):       
+        if numero % i == 0:#es par (no es primo)
             contador += 1
-    if contador == 0:
-        return True
+        if contador == 0:# es primo( por eso no incremento el contador)
+            return True
     else:
         return False
 def run ():
@@ -17,3 +15,8 @@ def run ():
         print("No es primo")
 if __name__ == "__main__":
     run()
+""" Hey ! 
+
+simplemente borre unas lineas que no eran necesarias y empece el rango desde 2
+para poder hacer la validacion y que no me tome 1 como primo (cuando no es asi) y asi
+el ejercicio quedo un poco mas corto y facil de leer """
