25d7b0437565b76007bf390bf0cbf7561b710736
Jose Antonio Rojas <game4droidplay@gmail.com>
2020-12-22 13:50:54 -0400

    Nuevo proyecto de NIF

--- prueba_primalidad.py
+++ prueba_primalidad.py
@@ -1,6 +1,6 @@
 def es_primo (numero):
     contador = 0
-    for i in range (2, numero + 1):       
+    for i in range (2, numero + 1):
         if numero % i == 0:#es par (no es primo)
             contador += 1
         if contador == 0:# es primo( por eso no incremento el contador)
@@ -15,7 +15,7 @@ def run ():
         print("No es primo")
 if __name__ == "__main__":
     run()
-""" Hey ! 
+""" Hey !
 
 simplemente borre unas lineas que no eran necesarias y empece el rango desde 2
 para poder hacer la validacion y que no me tome 1 como primo (cuando no es asi) y asi
